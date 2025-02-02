086b230a8476d7fa287aa000d75efc6d9ce9dfd2
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-02 22:05:40 +0600

    Initial Commit

--- app/src/main/java/com/sifat/translationapp/MainActivity.kt
+++ app/src/main/java/com/sifat/translationapp/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.sifat.translationapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.Translator
+import com.google.mlkit.nl.translate.TranslatorOptions
+import com.sifat.translationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var translator: Translator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.ENGLISH)
+            .setTargetLanguage(TranslateLanguage.BENGALI)
+            .build()
+        translator = Translation.getClient(options)
+
+        val conditions = DownloadConditions.Builder()
+            .requireWifi()
+            .build()
+
+        // **মডেল ডাউনলোড করো**
+        translator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                binding.translateButton.setOnClickListener {
+                    val textToTranslate = binding.inputText.text.toString()
+                    if (textToTranslate.isNotEmpty()) {
+                        translateText(textToTranslate, binding.outputText)
+                    } else {
+                        binding.outputText.text = "Please enter text to translate"
+                    }
+                }
+            }
+            .addOnFailureListener {
+                binding.outputText.text = "Model Download Failed"
+            }
+    }
+
+    private fun translateText(inputText: String, outputText: TextView) {
+        translator.translate(inputText)
+            .addOnSuccessListener { translatedText ->
+                outputText.text = translatedText
+            }
+            .addOnFailureListener {
+                outputText.text = "Translation Failed"
+            }
     }
 }
